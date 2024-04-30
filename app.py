dd1edc27427d5440b59ddbba692e6984806399f3
alz425 <135659809+alz425@users.noreply.github.com>
2024-04-29 20:28:18 -0400

    Feature and Model Selection

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from sklearn.feature_selection import (
     f_classif,
 )
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import Lasso, LassoCV, LogisticRegression
+from sklearn.linear_model import Lasso, LassoCV, LogisticRegression, Ridge
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
     DetCurveDisplay,
@@ -45,8 +45,7 @@ from sklearn.preprocessing import (
 )
 from sklearn.svm import LinearSVC
 import streamlit as st
-from sklearn import tree
-import graphviz
+
 
 set_config(display="diagram")  # display='text' is the default
 
@@ -91,51 +90,76 @@ def custom_prof_score(y, y_pred, roa=0.02, haircut=0.20):
 
 prof_score = make_scorer(custom_prof_score)
 
-dont_use = ["member_id", "id", "desc", "earliest_cr_line", "emp_title", "issue_d","title"]
-
 # list of all num vars:
 num_pipe_features = X_train.select_dtypes(include="number").columns
 
-# exclude any bad features:
-num_pipe_features = [e for e in num_pipe_features if e not in dont_use]
+# List of all categorical variables
+cat_pipe_features = X_train.select_dtypes(include='object').columns  # all: X_train.select_dtypes(include='object').columns
 
-cat_pipe_features = ["grade"]  # all: X_train.select_dtypes(include='object').columns
 
 ##################################################
+# Function to create a pipeline based on user-selected model and features
+def create_pipeline(model_name, num_pipe_features, cat_pipe_features):
+    if model_name == 'Logistic Regression':
+        clf = LogisticRegression(class_weight='balanced')
+    elif model_name == 'Random Forest':
+        clf = RandomForestClassifier(class_weight='balanced')
+    # Add more elif statements for other models
+    elif model_name == 'Lasso':
+        clf = Lasso(alpha = 0.3)
+    elif model_name == 'Ridge':
+        clf = Ridge()
+    # Preprocessing pipelines for numerical and categorical features
+    numer_pipe = make_pipeline(SimpleImputer(strategy="mean"), StandardScaler())
 
-numer_pipe = make_pipeline(SimpleImputer(strategy="mean"), StandardScaler())
-
-cat_pipe = make_pipeline(OneHotEncoder())
-
-
-# didn't use make_column_transformer; wanted to name steps
-preproc_pipe = make_column_transformer(
+    cat_pipe = make_pipeline(OneHotEncoder())
+    
+    # Preprocessing pipeline for the entire dataset
+    # didn't use make_column_transformer; wanted to name steps
+    preproc_pipe = make_column_transformer(
     (numer_pipe, num_pipe_features), 
     (cat_pipe, cat_pipe_features), 
     remainder="drop",
-)
+    )
 
-# I used "Pipeline" not "make_pipeline" bc I wanted to name the steps
-pipe = Pipeline([('columntransformer',preproc_pipe),
+    # I used "Pipeline" not "make_pipeline" bc I wanted to name the steps
+    pipe = Pipeline([('columntransformer',preproc_pipe),
                  ('feature_create','passthrough'), 
                  ('feature_select','passthrough'), 
-                 ('clf', LogisticRegression(class_weight='balanced'))
+                 ('clf', clf)
                 ])
+
+    return pipe
+
+
+
 '''
 hi
 '''
 
 ##################################################
 # begin : user choices
+st.title("Choose Model and Features, and Display Pipeline")
+# num_pipe_features =  .... st.menu(list of choices or something);
 
-# num_pipe_features =  .... st.menu(list of choices or something)
+# Checkbox to select numerical features
+selected_num_features = st.multiselect("Select numerical features:", num_pipe_features)
+
+# Checkbox to select categorical features
+selected_cat_features = st.multiselect("Select categorical features:", cat_pipe_features)
+    
+# Dropdown menu to choose the model
+model_name = st.selectbox("Choose Model:", ['Logistic Regression', 'Random Forest'])
+st.write("Selected Model:", model_name)
+
+# Create the pipeline based on the selected model and features
+pipe = create_pipeline(model_name, selected_num_features, selected_cat_features)
 
 # end: user choices
 ##################################################
 
 # pipe.set_param() # replace the vars eith the vars they want)
 # pipe.set_param() # replace the model with the model they choose
-
 
 pipe
 
